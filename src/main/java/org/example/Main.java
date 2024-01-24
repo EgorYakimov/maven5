98b3fa566f71c6a0e7a6eb8a90fe08605d784d01
EgorYakimov <151763291+EgorYakimov@users.noreply.github.com>
2024-01-24 21:39:02 +0500

    chng Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BonusService service = new BonusService();
+        long expected = 150;
+        long actual = service.calculate(5000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        expected = 50;
+        actual = service.calculate(5000, true);
+        System.out.println("2. " + expected + " == ? == " + actual);
     }
 }
